d98e4803ea432bdce4dec688d0d3f6fbdd652cf7
Jagadish K <jagadish2k2006@gmail.com>
2026-09-05 17:18:02 +0530

    fix(launcher): update RUN-APP.bat to target v2.5.3 build

--- RUN-APP.bat
+++ RUN-APP.bat
@@ -10,6 +10,21 @@ taskkill /F /IM "REDDOT-Workstation-OS.exe" /T >nul 2>&1
 ping 127.0.0.1 -n 2 >nul
 
 echo [2/3] Launching REDDOT Workstation OS...
+if exist "%~dp0app\v2.5.3\win-unpacked\REDDOT Workstation OS.exe" (
+  cd /d "%~dp0app\v2.5.3\win-unpacked"
+  start "" "REDDOT Workstation OS.exe"
+  echo.
+  echo [SUCCESS] REDDOT Workstation v2.5.3 launched successfully!
+  echo Window is opening on your desktop screen now...
+  ping 127.0.0.1 -n 2 >nul
+  exit /b 0
+)
+
+if exist "%~dp0app\v2.5.3\REDDOT-Workstation-OS-Portable.exe" (
+  start "" "%~dp0app\v2.5.3\REDDOT-Workstation-OS-Portable.exe"
+  exit /b 0
+)
+
 if exist "%~dp0app\v2.5.1\REDDOT-Workstation-OS-Portable\REDDOT-Workstation-OS.exe" (
   cd /d "%~dp0app\v2.5.1\REDDOT-Workstation-OS-Portable"
   start "" "REDDOT-Workstation-OS.exe"
